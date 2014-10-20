1b10664fe4974272ba7a3f23a53f74e13e8861d9
Dmitry Yu Okunev <dyokunev@ut.mephi.ru>
2014-10-20 12:34:42 +0400

    Forgot to update project_controllers_patch.rb. Fixed

--- lib/redmine_createsubproject_button/patches/project_controllers_patch.rb
+++ lib/redmine_createsubproject_button/patches/project_controllers_patch.rb
@@ -1,1 +1,24 @@
+module RedmineCreateSubProjectButton
+	module Patches
+		module ProjectsControllerPatch
+			def self.included(base)
+				base.class_eval do
+					unloadable
+
+					def new_with_create_subproject_button
+						Rails.logger.info('TEST:')
+						new_without_create_subproject_button
+						if params[:parent_id].nil? && !params[:id].nil?
+							params[:parent_id] = params[:id]
+						end
+					end
+					alias_method_chain :new, :create_subproject_button
+				end
+			end
+			module InstanceMethods
+			end
+		end
+	end
+end
+
 ProjectsController.send(:include, RedmineCreateSubProjectButton::Patches::ProjectsControllerPatch)
